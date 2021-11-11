8407189d61aa10607cc8122929466aa7bbff0ffb
thesachinjat <sachin.1choudhary@gmail.com>
2021-11-11 11:00:20 +0530

    end of color my view

--- app/src/main/java/com/example/colormyview/MainActivity.kt
+++ app/src/main/java/com/example/colormyview/MainActivity.kt
@@ -4,9 +4,10 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,21 +15,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setListeners()
     }
+
     private fun setListeners() {
-        val boxOne = findViewById<TextView>(R.id.box_one)
-        val boxTwo = findViewById<TextView>(R.id.box_two_text)
-        val boxThree = findViewById<TextView>(R.id.box_three_text)
-        val boxFour = findViewById<TextView>(R.id.box_four_text)
-        val boxFive = findViewById<TextView>(R.id.box_five_text)
-        val constraintL = findViewById<ConstraintLayout>(R.id.constraint_layout)
+
+
+
+
 
         val clickableViews: List<View> =
-                 listOf(boxOne, boxTwo, boxThree, boxFour, boxFive, constraintL)
-                 for (item in clickableViews) {
-                     item.setOnClickListener { makeColored(it)   }
-                 }
+            listOf(
+                box_one,
+                box_two_text,
+                box_three_text,
+                box_four_text,
+                box_five_text,
+                constraint_layout,
+                red_button,
+                yellow_button,
+                green_button
+            )
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
 
     }
+
     private
     fun makeColored(view: View) {
         when (view.id) {
@@ -41,6 +52,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
